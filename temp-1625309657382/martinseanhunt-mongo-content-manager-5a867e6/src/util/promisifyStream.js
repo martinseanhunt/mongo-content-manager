7b36f83ccec8a8e42a4733165675d02e8b4a3b52
Martin Hunt <martin.hunt@iohk.io>
2021-07-03 12:46:55 +0100

    remove temp folders

--- temp-1625309657382/martinseanhunt-mongo-content-manager-5a867e6/src/util/promisifyStream.js
+++
@@ -1,7 +0,0 @@
-const promisifyStream = (stream) =>
-  new Promise(function (resolve, reject) {
-    stream.on('close', () => resolve())
-    stream.on('error', reject)
-  })
-
-module.exports = { promisifyStream }

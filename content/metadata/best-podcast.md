c82da3add7abc80cef8954d93becd72818c283f7
Martin Hunt <martin.hunt@iohk.io>
2021-07-04 06:25:59 +0100

    catch errors for inividual entries

--- content/metadata/best-podcast.md
+++ content/metadata/best-podcast.md
@@ -2,7 +2,8 @@
 content_type: podcast
 title: What is the best podcast to learn about Cardano
 tags: ['learning', 'podcast', 'resource']
+url: https://www.youtube.com/watch?v=FKh8hjJNhWc&t
+image: infinity.jpg
 image_text: Lex Fridamns pod was great
-image_filename: infinity.jpg
 body_content: Lex Fridamns pod was great
 ---
--- src/syncContent.js
+++ src/syncContent.js
@@ -63,75 +63,80 @@ const syncContent = async () => {
 
   // Iterate over the filenames
   for (const filename of filenames) {
-    // Read the file contents - this will be a markdown file
-    const markdown = fs.readFileSync(`${metadataPath}/${filename}`, 'utf8')
+    try {
+      // Read the file contents - this will be a markdown file
+      const markdown = fs.readFileSync(`${metadataPath}/${filename}`, 'utf8')
 
-    // Parse the metadata contained in the markdown file and get the relevant fields
-    const {
-      metadata: {
+      // Parse the metadata contained in the markdown file and get the relevant fields
+      const {
+        metadata: {
+          title,
+          image,
+          image_text,
+          tags,
+          body_content,
+          content_type,
+          link_url,
+          video_url,
+        },
+      } = parseMD(markdown)
+
+      // create or update each entry in our database from each item in the repo.
+      // TODO: For the MVP version of the app we're using image hosting directly with github but we will need to come
+      // up with a better solution that doesn't rely on the images being stored in the repository. We will max out
+      // the repo size limit otherwise.
+
+      // first, try to find an entry with the filename
+      const dbItem = await Item.findOne({ filename })
+      const parsedItem = {
         title,
-        image,
-        image_text,
-        tags,
-        body_content,
-        content_type,
-        link_url,
-        video_url,
-      },
-    } = parseMD(markdown)
+        tags: tags,
+        contentType: content_type,
+        // Set optional fields to null if empty for comparison to existing entry
+        bodyContent: body_content || null,
+        videoUrl: video_url || null,
+        linkUrl: link_url || null,
+        image: image || null,
+        imageText: image_text || null,
+      }
 
-    // create or update each entry in our database from each item in the repo.
-    // TODO: For the MVP version of the app we're using image hosting directly with github but we will need to come
-    // up with a better solution that doesn't rely on the images being stored in the repository. We will max out
-    // the repo size limit otherwise.
+      if (!dbItem) {
+        // This is a new item so we'll build it and save
+        await new Item(parsedItem).save()
+        console.log(`Added: ${filename}`)
+      } else {
+        // The item exists in the DB so let's see if the item has changed in any way
 
-    // first, try to find an entry with the filename
-    const dbItem = await Item.findOne({ filename })
-    const parsedItem = {
-      title,
-      tags: tags,
-      contentType: content_type,
-      // Set optional fields to null if empty for comparison to existing entry
-      bodyContent: body_content || null,
-      videoUrl: video_url || null,
-      linkUrl: link_url || null,
-      image: image || null,
-      imageText: image_text || null,
-    }
+        // iterate over the parsed Items keys
+        let hasChanged = false
+        for (const itemKey in parsedItem) {
+          // If the property on the the parsed item is different from the record in the db we know we need to update
+          // the record so set hasChanged to true. We stringify the values so that the array of tags will be compared
+          // by value, not reference.
 
-    if (!dbItem) {
-      // This is a new item so we'll build it and save
-      await new Item(parsedItem).save()
-      console.log(`Added: ${filename}`)
-    } else {
-      // The item exists in the DB so let's see if the item has changed in any way
+          // Use isEqual from lodash so we can deep compare array of tags
+          if (!_.isEqual(parsedItem[itemKey], dbItem[itemKey])) {
+            console.log('Field changed:', parsedItem[itemKey], dbItem[itemKey])
+            hasChanged = true
+          }
+        }
 
-      // iterate over the parsed Items keys
-      let hasChanged = false
-      for (const itemKey in parsedItem) {
-        // If the property on the the parsed item is different from the record in the db we know we need to update
-        // the record so set hasChanged to true. We stringify the values so that the array of tags will be compared
-        // by value, not reference.
+        // update the record in the DB if it's changed
+        if (hasChanged) {
+          dbItem.filename = filename
+          dbItem.title = title
+          dbItem.image_filename = image_filename
+          dbItem.image_text = image_text
+          dbItem.tags = tags
+          dbItem.body_content = body_content
 
-        // Use isEqual from lodash so we can deep compare array of tags
-        if (!_.isEqual(parsedItem[itemKey], dbItem[itemKey])) {
-          console.log('Field changed:', parsedItem[itemKey], dbItem[itemKey])
-          hasChanged = true
+          await dbItem.save()
+          console.log(`Updated: ${filename}`)
         }
       }
-
-      // update the record in the DB if it's changed
-      if (hasChanged) {
-        dbItem.filename = filename
-        dbItem.title = title
-        dbItem.image_filename = image_filename
-        dbItem.image_text = image_text
-        dbItem.tags = tags
-        dbItem.body_content = body_content
-
-        await dbItem.save()
-        console.log(`Updated: ${filename}`)
-      }
+    } catch (e) {
+      console.error(e)
+      console.error(`error processing entry: ${filename}`)
     }
   }
 
